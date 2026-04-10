a6e7e001697f531e745c5901e57aabcf05045108
David Jokinen <18593830+jokinend@users.noreply.github.com>
2026-04-10 18:45:55 +0100

    Delete `dist/` directory

--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Timeline from './components/Timeline.vue';
-export { Timeline };
-export * from './components/Timeline.vue';
-export type { TimelineGroup, TimelineItem, TimelineItemBase, TimelineItemRange, TimelineItemPoint, TimelineItemBackground, TimelineMarker, TimelineBaseUnits, TimelineScale, TimelineScales, } from './types/timeline.ts';
